7403b0b87f05c36aec1082fb34bb1b3fbf799422
Giorgio Pizzorni <giorgio@pizzorni.com>
2016-03-01 10:36:55 -0500

    made date arguments optional and lists. Made either year/month/day OR all required

--- pbsacc.py
+++ pbsacc.py
@@ -1,14 +1,24 @@
-#!/usr/bin/python
-
 import argparse
+import re
 
 parser = argparse.ArgumentParser()
-parser.add_argument("year", type = int)
-parser.add_argument("month", type = int)
-parser.add_argument("day", type = int)
-parser.add_argument("--all", help = "use all log files in current 
-                    directory", action = "store_true")
+parser.add_argument("-y","--year", nargs = '*', type = int, 
+                    help = "list of years")
+parser.add_argument("-m","--month", nargs = '*', type = int,
+                    help = "list of months")
+parser.add_argument("-d","--day", nargs = '*', type = int,
+                    help = "list of days")
+parser.add_argument("-A","--all", help = "use all log files", 
+                    action = "store_true")
 args = parser.parse_args()
 
+if not (args.year or args.month or args.day or args.all):
+  parser.error('No dates specified, add year month day or --all')
 
+if (args.year):
+  year = args.year
+if(args.month):
+  month = args.month
+if(args.day):
+  day = args.day
 
