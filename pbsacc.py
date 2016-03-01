3eea148ee98650072b814385a4202caa8f90012c
Giorgio Pizzorni <giorgio@pizzorni.com>
2016-03-01 11:33:44 -0500

    added list of users positional arg

--- pbsacc.py
+++ pbsacc.py
@@ -8,6 +8,8 @@ parser.add_argument("-m","--month", nargs = '*', type = int,
                     help = "list of months")
 parser.add_argument("-d","--day", nargs = '*', type = int,
                     help = "list of days")
+parser.add_argument("-u", "--user", nargs = '*', type = str,
+                    help = "list of users")
 parser.add_argument("-A","--all", help = "use all log files", 
                     action = "store_true")
 args = parser.parse_args()
@@ -17,8 +19,9 @@ if not (args.year or args.month or args.day or args.all):
 
 if (args.year):
   year = args.year
-if(args.month):
+if (args.month):
   month = args.month
-if(args.day):
+if (args.day):
   day = args.day
-
+if (args.user):
+  user = args.user
